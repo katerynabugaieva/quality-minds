4968c0b663e6593f756df413571c7d0f4baaee95
Kateryna Bugaieva <sonnensmile@gmail.com>
2023-07-01 17:57:08 +0200

    newscomponent

---
+++ src/app/news/news.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      news works!
+    </p>
+  `,
+  styleUrl: './news.component.css'
+})
+export class NewsComponent {
+
+}
