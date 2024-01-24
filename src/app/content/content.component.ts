42cbf6436eda5933f4e0ea62bdd1f779ceaafec6
Kateryna Bugaieva <sonnensmile@gmail.com>
2024-01-24 16:55:21 +0100

    content.component.ts

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      app-content works!
+    </p>
+  `,
+  styleUrl: './content.component.css'
+})
+export class AppContentComponent {
+
+}
