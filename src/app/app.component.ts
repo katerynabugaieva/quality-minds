726f35282e8ed7b5f3cfe0e1c5172de8571dd604
Kateryna Bugaieva <sonnensmile@gmail.com>
2024-02-07 16:53:44 +0100

    component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { AppHeader } from './header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Housinglocation } from './housinglocation';
+import { CommonModule } from '@angular/common';
+import { AppContentComponent } from './content/content.component';
+import {  NewsComponent} from "./news/news.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppHeader, CommonModule, HousingLocationComponent, AppContentComponent, NewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'quality-minds';
+}
+
+
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
