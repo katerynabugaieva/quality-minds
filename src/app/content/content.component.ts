7eaa955fd44ae8d615cf02599dd4c74eb35d54e9
Kateryna Bugaieva <sonnensmile@gmail.com>
2024-02-19 12:40:41 +0100

    styles

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -5,9 +5,28 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      app-content works!
-    </p>
+    <section>
+     <div class="et_pb_section et_pb_section_0_tb_body et_pb_with_background et_section_regular">
+				<div class="et_pb_row et_pb_row_0_tb_body et_pb_equal_columns">
+				<div class="et_pb_column et_pb_column_4_4 et_pb_column_0_tb_body  et_pb_css_mix_blend_mode_passthrough et-last-child">
+				<div class="et_pb_module et_pb_blurb et_pb_blurb_0_tb_body service-header  et_pb_text_align_left  et_pb_blurb_position_left et_pb_bg_layout_dark">
+				<div class="et_pb_blurb_content">
+					<div class="et_pb_blurb_container">
+						<h1 class="et_pb_module_header"><span>News &amp; Blog</span></h1>
+						<div class="et_pb_blurb_description"><p>ABOUT US</p></div>
+					</div>
+				</div>
+			</div>
+			</div>
+				
+				
+				
+				
+			</div>
+				
+				
+			</div>
+</section>
   `,
   styleUrl: './content.component.css'
 })
